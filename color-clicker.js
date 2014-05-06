9069023aa4d3ccb33b76cfef144e3b37a0379ed2
Joy Paas <gxa.joy@gmail.com>
2014-05-06 10:59:08 +0800

    Added plugin functions

--- color-clicker.js
+++ color-clicker.js
@@ -1,5 +1,43 @@
 (function($) {
   $.fn.colorClicker = function(){
-    alert("colorClicker plugin activated");
+    var element = $(this);
+
+    element.on('click', function() {
+      bgColor = randomColor();
+
+      element.css('background', bgColor);
+      element.css('color', invertColor(bgColor));
+      element.text('Current color = ' + bgColor);
+    }).click();
+
+    // For chaining
+    return element;
+  }
+
+  function randomColor() {
+    var color = '#';
+
+    for(i = 0; i < 3; i++){
+      component = Math.floor(Math.random() * 256);
+
+      if(component < 16) color += '0';
+      // Convert to hex
+      color += component.toString(16);
+    }
+
+    return color;
+  }
+
+  function invertColor(color) {
+    var inverse = "#";
+
+    for(i = 1; i < 6; i += 2) {
+      inverseComponent = 256 - parseInt(color.substring(i, i + 2), 16);
+      if(inverseComponent < 16) inverse += '0';
+      // Convert to hex
+      inverse += inverseComponent.toString(16);
+    }
+
+    return inverse;
   }
 }(jQuery));
